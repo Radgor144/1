102d00de3bc15accd63a8b7454ad695821719515
Radgor144 <106109773+Radgor144@users.noreply.github.com>
2022-05-23 20:45:42 +0200

    Add files via upload

---
+++ 2Strona/skrypt.js
@@ -0,0 +1,13 @@
+let liczba = document.getElementsByClassName("liczba");
+let operator = document.getElementsByClassName("operator");
+let clear = document.getElementsByClassName("clear");
+let del = document.getElementsByClassName("del");
+let tablica = document.getElementsByClassName("tablica");
+let wynik = document.getElementsByClassName("wynik");
+
+let aktualneDzialanie = "";
+let poprzednieDzialanie = "";
+let operacja = undefined
+
+
+
